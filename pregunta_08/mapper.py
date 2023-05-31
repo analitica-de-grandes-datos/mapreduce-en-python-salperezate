010b8932dd15119c73c32303fa29dc6dcecc72b9
salperezate <salperezate@unal.edu.co>
2023-05-30 20:58:18 -0500

    avance

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,8 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    for line in sys.stdin:
+        line = line.strip().split('   ')
+        sys.stdout.write('{},{}\n'.format(line[0],line[2]))
--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,8 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    for line in sys.stdin:
+        key, val, val2 = line.strip().split("   ")
+        sys.stdout.write("{},{},{}\n".format(key, val, val2))
