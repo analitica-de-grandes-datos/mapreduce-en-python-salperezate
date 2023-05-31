c86cc63654a6edd0e527f6d214595e7963c8bfe6
salperezate <salperezate@unal.edu.co>
2023-05-30 20:15:06 -0500

    pregunta_03

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    for line in sys.stdin:
+        line = line.strip().split(",")
+        line[1] = int(line[1])
+        sys.stdout.write("{},{}\n".format(line[0],line[1]))
