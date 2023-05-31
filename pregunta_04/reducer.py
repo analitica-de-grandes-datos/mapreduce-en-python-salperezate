638f487d0464a6dffe0f79231cf679763e6014f8
salperezate <salperezate@unal.edu.co>
2023-05-30 20:29:31 -0500

    correccion

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,14 +1,11 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-import sys
+mport sys       
 if __name__ == '__main__':
-    lista = []
+    l = []  
     for line in sys.stdin:
-        line = line.strip()
-        line = line.split()
-        lista.append(line)
-    s_lista =set([x[0] for x in lista])
-    s_lista = sorted(s_lista, key=lambda x: x[0])
-    for i in s_lista:
-        sys.stdout.write("{},{}\n".format(i.lista.count([i])))
+        line = line.strip().split()
+        l.append(line)
+    for i in sorted(set([i[0] for i in l]),key=lambda x: x[0]):
+        sys.stdout.write('{},{}\n'.format(i,l.count([i])))
